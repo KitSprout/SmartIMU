2822ccebf5ed8b82edfb764c9c61e2fa175bbde6
Hom-Wang <wenhom.wang@gmail.com>
2018-04-22 12:25:04 +0800

    Add STM_Peripheral_GPIO 04/22

---
+++ firmware/STM_Peripheral_GPIO/Program/drivers/stm32f4_msp.c
@@ -0,0 +1,112 @@
+/**
+ *      __            ____
+ *     / /__ _  __   / __/                      __  
+ *    / //_/(_)/ /_ / /  ___   ____ ___  __ __ / /_ 
+ *   / ,<  / // __/_\ \ / _ \ / __// _ \/ // // __/ 
+ *  /_/|_|/_/ \__//___// .__//_/   \___/\_,_/ \__/  
+ *                    /_/   github.com/KitSprout    
+ * 
+ *  @file    stm32f4_msp.c
+ *  @author  KitSprout
+ *  @date    22-Apr-2018
+ *  @brief   
+ * 
+ */
+
+/* Includes --------------------------------------------------------------------------------*/
+#include "drivers\stm32f4_system.h"
+
+/** @addtogroup STM32_Driver
+ *  @{
+ */
+
+/* Define ----------------------------------------------------------------------------------*/
+/* Macro -----------------------------------------------------------------------------------*/
+/* Typedef ---------------------------------------------------------------------------------*/
+/* Variables -------------------------------------------------------------------------------*/
+/* Prototypes ------------------------------------------------------------------------------*/
+/* Functions -------------------------------------------------------------------------------*/
+
+/*                             [ HSx          USB     ]
+System Clock source            = PLL
+SYSCLK(Hz)                     = 100000000    96000000
+HCLK(Hz)                       = 100000000    96000000
+AHB Prescaler                  = 1
+APB1 Prescaler                 = 2
+APB2 Prescaler                 = 1
+HSx Frequency(Hz)              = 16000000
+PLL_M                          = 16
+PLL_N                          = 400          384
+PLL_P                          = 4            4
+PLL_Q                          = 7            8
+VDD(V)                         = 3.3
+Main regulator output voltage  = Scale2
+Flash Latency(WS)              = 3
+*/
+
+void HAL_MspInit( void )
+{
+  RCC_ClkInitTypeDef RCC_ClkInitStruct;
+  RCC_OscInitTypeDef RCC_OscInitStruct;
+
+  __HAL_RCC_PWR_CLK_ENABLE();
+  __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE1);
+
+#if KS_HW_HCLOCK_SOUCE
+  /* Enable HSE Oscillator and activate PLL with HSE as source */
+  RCC_OscInitStruct.OscillatorType      = RCC_OSCILLATORTYPE_HSE;
+  RCC_OscInitStruct.HSEState            = RCC_HSE_ON;
+  RCC_OscInitStruct.PLL.PLLState        = RCC_PLL_ON;
+  RCC_OscInitStruct.PLL.PLLSource       = RCC_PLLSOURCE_HSE;
+  RCC_OscInitStruct.PLL.PLLM            = HSE_VALUE / 1000000;
+
+#else
+  /* Enable HSI Oscillator and activate PLL with HSI as source */
+  RCC_OscInitStruct.OscillatorType      = RCC_OSCILLATORTYPE_HSI;
+  RCC_OscInitStruct.HSIState            = RCC_HSI_ON;
+  RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
+  RCC_OscInitStruct.PLL.PLLState        = RCC_PLL_ON;
+  RCC_OscInitStruct.PLL.PLLSource       = RCC_PLLSOURCE_HSI;
+  RCC_OscInitStruct.PLL.PLLM            = HSI_VALUE / 1000000;
+
+#endif
+
+#if KS_FW_USB_ENABLE
+  RCC_OscInitStruct.PLL.PLLN            = 384;
+  RCC_OscInitStruct.PLL.PLLP            = RCC_PLLP_DIV4;
+  RCC_OscInitStruct.PLL.PLLQ            = 8;
+
+#else
+  RCC_OscInitStruct.PLL.PLLN            = 400;
+  RCC_OscInitStruct.PLL.PLLP            = RCC_PLLP_DIV4;
+  RCC_OscInitStruct.PLL.PLLQ            = 7;
+
+#endif
+
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK) {
+    while (1) { ; }
+  }
+
+  /* Select PLL as system clock source and configure the HCLK, PCLK1 and PCLK2 clocks dividers */
+  RCC_ClkInitStruct.ClockType           = (RCC_CLOCKTYPE_SYSCLK | RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2);
+  RCC_ClkInitStruct.SYSCLKSource        = RCC_SYSCLKSOURCE_PLLCLK;
+  RCC_ClkInitStruct.AHBCLKDivider       = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider      = RCC_HCLK_DIV2;
+  RCC_ClkInitStruct.APB2CLKDivider      = RCC_HCLK_DIV1;
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_3) != HAL_OK) {
+    while(1) { ; }
+  }
+}
+
+#ifdef HAL_UART_MODULE_ENABLED
+void HW_BoardInformation( void )
+{
+  printf(" ---- BOARD INFO ------\r\n");
+  printf("  BOARD : %s\r\n", KS_HW_BOARD_NAME);
+  printf("  CHIP  : %s\r\n", KS_HW_MCU_NAME);
+  printf("  FREQ  : %i MHz\r\n", KS_SYSCLK / 1000000);
+  printf(" ----------------------\r\n\r\n");
+}
+#endif
+
+/*************************************** END OF FILE ****************************************/
